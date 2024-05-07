b680ce3d127a8157f63a33774330636d10a40d84
Aparicio Junior <apariciojunior11@gmail.com>
2024-05-07 17:22:06 -0300

    Endpoint get by id, personagem/:id criado

--- salvatore-academy-dev-frontend/crud-expressjs-memoria/index.js
+++ salvatore-academy-dev-frontend/crud-expressjs-memoria/index.js
@@ -18,6 +18,18 @@ app.get('/personagem', (req, res) => {
    res.send(lista); // o express já transforma uma variável em JSON
 });
 
+// Endpoint Read Single [GET]
+app.get('/personagem/:id', (req, res) => {
+  // Acessar o id que foi passado na URL  
+  const id = req.params.id;
+  
+  // Buscar o personagem na lista
+  const item = lista.find(item => item.id == id);
+
+  // Retornar o personagem encontrado
+  res.send(item);
+});
+
 app.listen(port, () => {
     console.log(`Server listening at http://localhost:${port}`);
 });
