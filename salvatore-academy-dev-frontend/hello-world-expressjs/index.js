dfb626cc743ac9f125cb4a183a15a0a1b8c43339
Aparicio Junior <apariciojunior11@gmail.com>
2024-05-07 15:56:46 -0300

    Endpoint /oi adicionado e criação do script start

--- salvatore-academy-dev-frontend/hello-world-expressjs/index.js
+++ salvatore-academy-dev-frontend/hello-world-expressjs/index.js
@@ -1,10 +1,15 @@
 const express = require('express');
-
 const app = express();
+
 const port = 3000;
 
 app.get('/', (req, res) => {
     res.send('Hello World!');
+});
+
+// endpiint oi
+app.get('/oi', (req, res) => {
+    res.send('Oi!');
 });
 
 app.listen(port, () => {
