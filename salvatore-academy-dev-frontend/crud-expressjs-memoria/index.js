cbb28528cbd45523da49c9a8bed1346137fcb684
Aparicio Junior <apariciojunior11@gmail.com>
2024-05-07 17:14:04 -0300

    Endpoint readall [GET] /personagem criado

--- salvatore-academy-dev-frontend/crud-expressjs-memoria/index.js
+++ salvatore-academy-dev-frontend/crud-expressjs-memoria/index.js
@@ -7,6 +7,17 @@ app.get('/', (req, res) => {
     res.send('Hello World!!!!');
 });
 
+const lista = [
+    { id: 1, nome: 'João' },
+    { id: 2, nome: 'Maria' },
+    { id: 3, nome: 'José' },
+];
+
+// Endpoint Read All [GET]
+app.get('/personagem', (req, res) => {
+   res.send(lista); // o express já transforma uma variável em JSON
+});
+
 app.listen(port, () => {
     console.log(`Server listening at http://localhost:${port}`);
 });
